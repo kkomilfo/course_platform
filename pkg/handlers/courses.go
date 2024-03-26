f4f24d88affd5081b51e83c3496e613b620b9f7d
kkomilfo <a.nactia1978@gmail.com>
2024-03-26 05:19:14 +0100

    improvments

--- pkg/handlers/courses.go
+++ pkg/handlers/courses.go
@@ -190,7 +190,7 @@ func (h *CourseHandler) GetSubjectTaskForStudent(w http.ResponseWriter, r *http.
 
 func (h *CourseHandler) GetAllEntrolledStudentsByCourseID(w http.ResponseWriter, r *http.Request) {
 	requestContext := r.Context().Value(RequestContextKey).(RequestContext)
-	if requestContext.Role != models.TeacherRole {
+	if requestContext.Role == models.AdministratorRole {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
